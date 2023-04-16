9b03a9098f3ccf196416cd395168402358df0078
Yohei Yasukawa <yohei@yasslab.jp>
2023-04-16 10:45:28 +0900

    Comment out controller test for hello#index

--- test/controllers/hello_codespaces_controller_test.rb
+++ test/controllers/hello_codespaces_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class HelloCodespacesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get "/"
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get "/"
+  #   assert_response :success
+  # end
 end
